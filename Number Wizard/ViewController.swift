178bce129732c93741c0b5ccc93d7d88e4b7ffa0
enginbora <enginbora678@gmail.com>
2022-06-24 05:02:49 +0300

    init

--- Number Wizard/ViewController.swift
+++ Number Wizard/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var minNumber = 0
+    var maxNumber = 1000
+    var guess = 0
+    @IBOutlet weak var guessText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guess = Int.random(in: minNumber...maxNumber)
+        updateGuessText()
     }
 
-
+    @IBAction func greaterButton(_ sender: Any) {
+        minNumber = guess + 1
+        calculateNextGuess()
+    }
+    
+    @IBAction func lowerButton(_ sender: Any) {
+        maxNumber = guess - 1
+        calculateNextGuess()
+    }
+    
+    func calculateNextGuess(){
+        guess = Int.random(in: minNumber...maxNumber)
+        updateGuessText()
+    }
+    
+    func updateGuessText(){
+        guessText.text = "Is it... \(guess)?"
+    }
+    
+    @IBAction func yesButton(_ sender: Any) {
+        guessText.text = "I win!"
+    }
 }
-
